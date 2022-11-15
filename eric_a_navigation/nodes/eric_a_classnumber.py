1bba0d55c0f5ac98a2338e56bd1de5c7923006e4
jeong-han-kim <kimjh9813@naver.com>
2022-11-15 15:12:24 +0900

    FEAT : Add emcl2 package code

---
+++ eric_a_navigation/nodes/eric_a_classnumber.py
@@ -0,0 +1,6 @@
+class classnumber():
+    # class number = (x,y,thetha)
+    class101 = (0,0,0)
+    class102 = (0,0,0)
+    class103 = (0,0,0)
+    class104 = (0,0,0)
---
+++ eric_a_navigation/nodes/eric_a_costmap_clear.py
@@ -0,0 +1,114 @@
+#!/usr/bin/env python3
+
+import rospy
+import rospkg
+from move_base_msgs.msg import *
+from tf.transformations import quaternion_from_euler
+from geometry_msgs.msg import Pose, Point, Quaternion
+from eric_a_navigation.srv import ClassNumber, ClassNumberResponse
+import actionlib
+import os
+from eric_a_classnumber import classnumber
+
+# class fileRoot(object):
+#     pkg_path = rospkg.RosPack()
+#     speak_path = pkg_path.get_path('eric_a_speak')
+    # web = pkg_path.get_path('hongdo_ros_web')
+
+
+
+# class SoundStart:
+#     _voice_path=None
+#     _sound_path=None
+
+#     def __init__(self, vocie_path = None, sound_path = None):
+#         self._voice_path=vocie_path
+#         self._sound_path=sound_path
+
+#     def play_voice(self, req):
+#         rospy.loginfo("voice :  %s"%req.sequence)
+#         os.system('play ' + self._voice_path +'/%s.mp3'%req.sequence)
+#         return PlaySongResponse('finish')
+
+#     def play_sound(self,req):
+#         rospy.loginfo("background sound :  %s"%req.sequence)
+#         os.system('play ' + self._sound_path +'/%s.mp3'%req.sequence)
+#         return PlaySongResponse('finish')
+
+    # def __del__(self):
+    #     self._voice_path=None
+    #     self._sound_path=None
+
+class EricAStartNode:
+    def __init__(self):
+        #sound
+        # voice_path = fileRoot.webconnect + '/speak/voice'
+        # sound_path = fileRoot.webconnect + '/speak/sound'
+
+        #storaging
+        # self.sound_start=SoundStart(voice_path, sound_path)
+        
+
+        ##service
+        rospy.Service('/classnumber',ClassNumber , self.purpose_class_set)
+        # rospy.Service('/play_voice', PlaySong, self.sound_start.play_voice)
+        
+        #action
+        self.goalclient = actionlib.SimpleActionClient('move_base/goal', move_base_msgs.msg.MoveBaseAction)
+        
+        #timer
+        # self.move_base_goal.header.stamp = rospy.Time.now()
+
+        self.goalclient.wait_for_server()
+        # self.opencvStart()
+        rospy.loginfo('Ready to eric_a_speak Node')
+        rospy.on_shutdown(self.__del__)
+
+    def purpose_class_set(self, req):
+        # self.classnum = classnumber.class
+        # self.classnum=[]
+        if req.number ==101:
+            self.classnum = classnumber.class101
+        elif req.number == 102:
+            self.classnum = classnumber.class102
+        elif req.number == 103:
+            self.classnum = classnumber.class103
+
+
+        return ClassNumberResponse('finish')
+
+    def update_nav_goal(self):
+        id_number = 0
+        x = self.classnum[0]
+        y = self.classnum[1]
+        thetha = self.classnum[2]
+        purpose = Point(x,y,0)
+        goal_orientation_quat = quaternion_from_euler(0,0, thetha)
+        
+        
+        move_base_goal = move_base_msgs.msg.MoveBaseActionGoal()
+
+        move_base_goal.header.stamp = rospy.Time.now()
+        move_base_goal.goal_id.id = id_number
+        move_base_goal.goal.target_pose.pose = Pose(Point(purpose),Quaternion(*goal_orientation_quat))
+        self.goalclient(move_base_goal)
+        self.goalclient.wait_for_result()
+        return self.goalclient.get_result()
+
+
+    def main(self):
+        rospy.loginfo("hi")
+        rospy.spin()
+
+    def __del__(self):
+        print("[info]terminating eric_a_speak_node")
+
+
+
+if __name__ == '__main__':
+    rospy.init_node('eric_a_start_node', anonymous=True)
+    node=EricAStartNode()
+    node.main()
+
+
+    #출발 직전 costmap clear, 목적지 goal, 출발명령받기
